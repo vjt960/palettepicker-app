487f4b932cf21fdb0d1cf318e1d1616423eb934b
Kevin Krato <kratok08@gmail.com>
2019-08-20 17:55:36 -0600

    Create ProjectContainer component and migrate in styles js

---
+++ src/components/ProjectContainer/ProjectContainer.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import "./ProjectContainer.css";
+
+export default class ProjectContainer extends Component {
+  render() {
+    return (
+      <section className="ProjectContainer">
+        <div className="project-title">
+          <h2>Alts -- send to steve</h2>
+        </div>
+        <div className="delete-wrapper">
+          <div className="project-delete">
+            <h2>X</h2>
+          </div>
+        </div>
+        <article className="Palette">
+          <h4 className="palette-title">An Especially Long Title</h4>
+          <div className="color" />
+          <div className="color" />
+          <div className="color" />
+          <div className="color" />
+          <div className="color" />
+        </article>
+        <article className="Palette">
+          <h4 className="palette-title">Untitled</h4>
+          <div className="color" />
+          <div className="color" />
+          <div className="color" />
+        </article>
+        <article className="Palette">
+          <h4 className="palette-title">Design Primary</h4>
+          <div className="color" />
+        </article>
+        <article className="Palette">
+          <h4 className="palette-title">Design Alt</h4>
+          <div className="color" />
+          <div className="color" />
+          <div className="color" />
+          <div className="color" />
+          <div className="color" />
+          <div className="color" />
+          <div className="color" />
+          <div className="color" />
+          <div className="color" />
+          <div className="color" />
+          <div className="color" />
+          <div className="color" />
+          <div className="color" />
+          <div className="color" />
+          <div className="color" />
+          <div className="color" />
+        </article>
+      </section>
+    );
+  }
+}
