8ab0b84964016196af0e91859278f13c970e178a
Kevin Krato <kratok08@gmail.com>
2019-08-20 17:55:46 -0600

    Import ProjectContainer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import PalettePicker from "./components/PalettePicker/PalettePicker";
+import ProjectContainer from "./components/ProjectContainer/ProjectContainer";
 
 function App() {
   return (
@@ -18,93 +19,9 @@ function App() {
           <article className="palettes-header">
             <p>This area is where your projects will be saved!</p>
           </article>
-          <section className="ProjectBox">
-            <div className="project-title">
-              <h2>Project 5</h2>
-            </div>
-            <div className="delete-wrapper">
-              <div className="project-delete">
-                <h2>X</h2>
-              </div>
-            </div>
-            <article className="Palette">Palette</article>
-            <article className="Palette">Palette</article>
-            <article className="Palette">Palette</article>
-          </section>
-          <section className="ProjectBox">
-            <div className="project-title">
-              <h2>Happy Colors!</h2>
-            </div>
-            <div className="delete-wrapper">
-              <div className="project-delete">
-                <h2>X</h2>
-              </div>
-            </div>
-            <article className="Palette">Palette</article>
-            <article className="Palette">Palette</article>
-          </section>
-          <section className="ProjectBox">
-            <div className="project-title">
-              <h2>Main Site colors</h2>
-            </div>
-            <div className="delete-wrapper">
-              <div className="project-delete">
-                <h2>X</h2>
-              </div>
-            </div>
-            <article className="Palette">
-              <h4 className="palette-title">Very Cool Long Title</h4>
-              <div className="color" />
-              <div className="color" />
-            </article>
-          </section>
-          <section className="ProjectBox">
-            <div className="project-title">
-              <h2>Alts -- send to steve</h2>
-            </div>
-            <div className="delete-wrapper">
-              <div className="project-delete">
-                <h2>X</h2>
-              </div>
-            </div>
-            <article className="Palette">
-              <h4 className="palette-title">An Especially Long Title</h4>
-              <div className="color" />
-              <div className="color" />
-              <div className="color" />
-              <div className="color" />
-              <div className="color" />
-            </article>
-            <article className="Palette">
-              <h4 className="palette-title">Untitled</h4>
-              <div className="color" />
-              <div className="color" />
-              <div className="color" />
-            </article>
-            <article className="Palette">
-              <h4 className="palette-title">Design Primary</h4>
-              <div className="color" />
-            </article>
-            <article className="Palette">
-              <h4 className="palette-title">Design Alt</h4>
-              <div className="color" />
-              <div className="color" />
-              <div className="color" />
-              <div className="color" />
-              <div className="color" />
-              <div className="color" />
-              <div className="color" />
-              <div className="color" />
-              <div className="color" />
-              <div className="color" />
-              <div className="color" />
-              <div className="color" />
-              <div className="color" />
-              <div className="color" />
-              <div className="color" />
-              <div className="color" />
-            </article>
-          </section>
+          <ProjectContainer />
+          {/* <ProjectContainer />
+          <ProjectContainer /> */}
         </section>
       </main>
     </div>
