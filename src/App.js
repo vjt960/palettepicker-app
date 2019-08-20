c898a510c6054f36c3817ffba83eab2802e7f356
Kevin Krato <kratok08@gmail.com>
2019-08-19 23:02:59 -0600

    Create and populate projects section

--- src/App.js
+++ src/App.js
@@ -11,7 +11,37 @@ function App() {
       <main className="MainPage">
         <PalettePicker vRotate={1} totalColors={25} />
         <section className="PalettesContainer">
+          <article className="palettes-header">
+            <p>This area is where your projects will be saved!</p>
+          </article>
           <section className="ProjectBox">
+            <div className="project-title">
+              <h2>Project 5</h2>
+            </div>
+            <article className="Palette">Palette</article>
+            <article className="Palette">Palette</article>
+            <article className="Palette">Palette</article>
+          </section>
+          <section className="ProjectBox">
+            <div className="project-title">
+              <h2>Happy Colors!</h2>
+            </div>
+            <article className="Palette">Palette</article>
+            <article className="Palette">Palette</article>
+          </section>
+          <section className="ProjectBox">
+            <div className="project-title">
+              <h2>Main Site colors</h2>
+            </div>
+            <article className="Palette">Palette</article>
+          </section>
+          <section className="ProjectBox">
+            <div className="project-title">
+              <h2>Alts -- send to steve</h2>
+            </div>
+            <article className="Palette">Palette</article>
+            <article className="Palette">Palette</article>
+            <article className="Palette">Palette</article>
             <article className="Palette">Palette</article>
           </section>
         </section>
