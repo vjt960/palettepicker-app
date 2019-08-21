1cad0524a5eda369a901b6443904990e66b7f039
Kevin Krato <kratok08@gmail.com>
2019-08-20 20:46:05 -0600

    Conver to functional component

--- src/components/Palette/Palette.js
+++ src/components/Palette/Palette.js
@@ -1,16 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Palette.css";
 
-export default class Palette extends Component {
-  render() {
-    const colors = this.props.colors.map(color => {
-      return <div className="color" style={{ backgroundColor: color }} />;
-    });
-    return (
-      <article className="Palette">
-        <h4 className="palette-title">{this.props.title}</h4>
-        {colors}
-      </article>
-    );
-  }
-}
+const Palette = props => {
+  const colors = props.colors.map(color => {
+    return <div className="color" style={{ backgroundColor: color }} />;
+  });
+  return (
+    <article className="Palette">
+      <h4 className="palette-title">{props.title}</h4>
+      {colors}
+    </article>
+  );
+};
+
+export default Palette;
