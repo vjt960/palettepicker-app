b5d450e765b2dea1e3a0eb5f7d8474074dc44972
Kevin Krato <kratok08@gmail.com>
2019-08-20 20:01:58 -0600

    Pass data through components

--- src/components/ProjectsContainer/ProjectsContainer.js
+++ src/components/ProjectsContainer/ProjectsContainer.js
@@ -4,16 +4,46 @@ import "./ProjectsContainer.css";
 
 export default class ProjectsContainer extends Component {
   state = {
-    projects: []
+    projects: [
+      {
+        projectTitle: "--Send to Steve",
+        palettes: [
+          {
+            paletteTitle: "Ok here is title",
+            paletteColors: ["#45CD12", "#DFD442", "#4E3211"]
+          },
+          { paletteTitle: "Idea - 2 Colors", paletteColors: ["#123456"] },
+          {
+            paletteTitle: "Serene",
+            paletteColors: ["#FEDCBA", "#45CD12", "#3A3A3A", "#123123"]
+          },
+          {
+            paletteTitle: "-- Send to Steve",
+            paletteColors: [
+              "#FEDCBA",
+              "#FEDFED",
+              "#ABCABC",
+              "#123123",
+              "#3A3A3A"
+            ]
+          }
+        ]
+      }
+    ]
   };
   render() {
+    const projects = this.state.projects.map(project => {
+      return (
+        <Project title={project.projectTitle} palettes={project.palettes} />
+      );
+    });
     return (
       <div>
         <section className="ProjectsContainer">
           <header className="projects-container-header">
             <p>This area is where your projects will be saved!</p>
           </header>
-          <Project />
+          {projects}
         </section>
       </div>
     );
