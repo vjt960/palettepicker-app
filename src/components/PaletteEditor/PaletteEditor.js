c5f6c35619b6f86ba515abaa0ca97ce36077657e
Kevin Krato <kratok08@gmail.com>
2019-08-22 16:01:42 -0600

    Redirect back on exit click

--- src/components/PaletteEditor/PaletteEditor.js
+++ src/components/PaletteEditor/PaletteEditor.js
@@ -1,12 +1,17 @@
 import React, { Fragment } from "react";
 import "./PaletteEditor.css";
 
-export default function PaletteEditor() {
+export default function PaletteEditor(props) {
+  const handleExit = () => {
+    props.history.goBack();
+  };
   return (
     <Fragment>
       <div className="screen" />
       <section className="PaletteEditor">
-        <button className="editor-exit">X</button>
+        <button className="editor-exit" onClick={handleExit}>
+          X
+        </button>
         <form className="editor-form" action="">
           <h3 className="editor-title">Title</h3>
           <label htmlFor="">Label 1</label>
