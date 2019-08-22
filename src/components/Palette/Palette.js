648a298228f0b93cd73aa3440de11fafaeabecbd
Kevin Krato <kratok08@gmail.com>
2019-08-22 16:00:59 -0600

    Redirect withRouter and console log on X btn click

--- src/components/Palette/Palette.js
+++ src/components/Palette/Palette.js
@@ -1,24 +1,33 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 import "./Palette.css";
 
 const Palette = props => {
+  const openEditor = () => {
+    props.history.push("edit-palette");
+  };
   const colors = props.colors.map((color, i) => {
     return (
       <div
         className="color"
         key={i}
         style={{ backgroundColor: color }}
-        onClick={() => console.log("open box")}
+        onClick={openEditor}
       />
     );
   });
   return (
     <article className="Palette">
-      <button className="palette-delete">X</button>
+      <button
+        className="palette-delete"
+        onClick={() => console.log("Delete me")}
+      >
+        X
+      </button>
       <h4 className="palette-title">{props.title}</h4>
       {colors}
     </article>
   );
 };
 
-export default Palette;
+export default withRouter(Palette);
