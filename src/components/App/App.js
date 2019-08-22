5bef0f873628eafc810c0995939474b25626bd1c
Kevin Krato <kratok08@gmail.com>
2019-08-22 16:09:55 -0600

    Handle routes and set position to relative

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import "./App.css";
 import LandingPage from "../LandingPage/LandingPage";
 import NavBar from "../NavBar/NavBar";
+import PaletteEditor from "../PaletteEditor/PaletteEditor";
 import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Route path="/" component={NavBar} />
-      <Route exact path="/" component={LandingPage} />
+      <Route exact path="/edit-palette" component={PaletteEditor} />
+      <Route path="/" component={LandingPage} />
     </div>
   );
 }
