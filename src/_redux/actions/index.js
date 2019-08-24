368c99b83b14ac84c3e566f7dfd1518a058383a3
Kevin Krato <kratok08@gmail.com>
2019-08-24 14:59:25 -0600

    Add additional actions

--- src/_redux/actions/index.js
+++ src/_redux/actions/index.js
@@ -4,3 +4,24 @@ export const addUserProjects = project => {
     project
   };
 };
+
+export const createNewProject = project => {
+  return {
+    type: "CREATE_NEW_PROJECT",
+    project
+  };
+};
+
+export const updateExistingProject = project => {
+  return {
+    type: "UPDATE_EXISTING_PROJECT",
+    project
+  };
+};
+
+export const updateCurrentPalette = palette => {
+  return {
+    type: "UPDATE_CURRENT_PALETTE",
+    palette
+  };
+};
