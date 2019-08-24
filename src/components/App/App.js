16949c8bba60dc92050bd3403ad2de7952cb5f8b
Kevin Krato <kratok08@gmail.com>
2019-08-24 14:57:48 -0600

    Add route path for SavePopup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import LandingPage from "../LandingPage/LandingPage";
 import NavBar from "../NavBar/NavBar";
 import PaletteEditor from "../PaletteEditor/PaletteEditor";
+import SavePopup from "../../containers/SavePopup/SavePopup";
 import { Route } from "react-router-dom";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Route path="/" component={NavBar} />
       <Route exact path="/edit-palette" component={PaletteEditor} />
+      <Route exact path="/save-palette" component={SavePopup} />
       <Route path="/" component={LandingPage} />
     </div>
   );
