7d1dda28b2b379e34ba4b12128cfaf48670205d4
Kevin Krato <kratok08@gmail.com>
2019-08-24 11:31:06 -0600

    Integrate redux into application

--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import "./index.css";
 import App from "./components/App/App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { rootReducer } from "./_redux/reducers/combineReducers";
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 const application = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.render(application, document.getElementById("root"));
