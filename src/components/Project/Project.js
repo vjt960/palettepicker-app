e038ae04b50c7b3698de24752966892d99968c32
Kevin Krato <kratok08@gmail.com>
2019-08-25 18:14:38 -0600

    Add Transition and GSAP implementation

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,6 +1,8 @@
 import React from "react";
-import "./Project.css";
 import Palette from "../Palette/Palette";
+import { TweenMax } from "gsap/all";
+import { Transition } from "react-transition-group";
+import "./Project.scss";
 
 const Project = props => {
   const palettes = props.palettes.map(palette => {
@@ -12,22 +14,40 @@ const Project = props => {
       />
     );
   });
+
+  const { in: show, removeProject, project } = props;
   return (
-    <section className="Project">
-      <div className="project-title">
-        <h2>{props.title}</h2>
-      </div>
-      <div className="dotted-line" />
-      <div className="delete-wrapper">
-        <button
-          onClick={() => console.log("Delete this Project")}
-          className="project-delete"
-        >
-          <h2>X</h2>
-        </button>
-      </div>
-      {palettes}
-    </section>
+    <Transition
+      timeout={1000}
+      mountOnEnter
+      unmountOnExit
+      appear
+      in={show}
+      addEndListener={(node, done) => {
+        TweenMax.to(node, 0.55, {
+          y: 0,
+          autoAlpha: show ? 1 : 0,
+          onComplete: done,
+          delay: !show ? 0 : project.init ? props.index * 0.15 : 0
+        });
+      }}
+    >
+      <section className="Project">
+        <div className="project-title">
+          <h2>{props.title}</h2>
+        </div>
+        <div className="dotted-line" />
+        <div className="delete-wrapper">
+          <button
+            onClick={() => removeProject(props.id)}
+            className="project-delete"
+          >
+            <h2>X</h2>
+          </button>
+        </div>
+        {palettes}
+      </section>
+    </Transition>
   );
 };
 
