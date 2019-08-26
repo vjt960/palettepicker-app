3c93e44d6ea2fb1d5147730a74af45e47885ba23
Turing School <turingschool@Turings-MacBook-Pro.local>
2019-08-26 16:21:41 -0600

    Add login and register route paths

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,15 +4,18 @@ import LandingPage from "../LandingPage/LandingPage";
 import NavBar from "../NavBar/NavBar";
 import PaletteEditor from "../PaletteEditor/PaletteEditor";
 import SavePopup from "../../containers/SavePopup/SavePopup";
-import { Route } from "react-router-dom";
+import userPopup from "../../containers/userPopup/userPopup";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Route path="/" component={NavBar} />
+      <Route path="/" component={LandingPage} />
       <Route exact path="/edit-palette" component={PaletteEditor} />
       <Route exact path="/save-palette" component={SavePopup} />
-      <Route path="/" component={LandingPage} />
+      <Route exact path="/login" component={userPopup} />
+      <Route exact path="/register" component={userPopup} />
     </div>
   );
 }
