fe53d6a3e594b0435de8c71e11e8a71151d68d61
Turing School <turingschool@Turings-MacBook-Pro.local>
2019-08-26 16:22:37 -0600

    Add router onclick functionality on component

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { withRouter } from 'react-router-dom';
 import "./NavBar.css";
 
-export default function NavBar() {
+function NavBar(props) {
+  const handleClick = (e,destination) => {
+    e.preventDefault();
+    props.history.push(`/${destination}`)
+  }
   return (
     <header className="NavBar">
       <h2>PalettePicker</h2>
       <form className="navbar-form">
-        <button>Login</button>
-        <button>Register</button>
+        <button onClick={(e) => handleClick(e,"login")}>Login</button>
+        <button onClick={(e) => handleClick(e,"register")}>Register</button>
       </form>
     </header>
   );
 }
+
+export default withRouter(NavBar);
