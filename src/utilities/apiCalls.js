99e649d4576a8a480f6ea2eabad7876d42dc22da
Vinton Teo <taulago@ymail.com>
2019-08-27 13:32:12 -0600

    Create utilities directory and write fetch calls in apiCalls.js file within.
    Write loginUser fetch function.
    
    Fix url in loginUser function.
    
    Write fetch function to create user.
    
    Write fetch function to get projects.
    
    Write fetch function to get single project.
    
    Write fetch function to create new project.
    
    Write fetch function to edit an existing project.
    
    Write fetch function to delete a project.
    
    Write fetch function to get palettes.
    
    Write fetch function to create a new palette.
    
    Write fetch function to delete an existing palette.

---
+++ src/utilities/apiCalls.js
@@ -0,0 +1,177 @@
+export const loginUser = async (username, password) => {
+  const url = 'http://localhost:3001';
+  const path = '/api/v1/users';
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ username, password })
+  };
+  try {
+    const response = await fetch(url + path, options);
+    if (!response.ok) {
+      const message = await response.json();
+      throw new Error(message.error);
+    }
+    return response.json();
+  } catch (error) {
+    throw Error(error.message);
+  }
+};
+
+export const createUser = async (username, password) => {
+  const url = 'http://localhost:3001';
+  const path = '/api/v1/users/new';
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ username, password })
+  };
+  const response = await fetch(url + path, options);
+  try {
+    if (!response.ok) {
+      const message = await response.json();
+      throw new Error(message.error);
+    }
+    const obj = await response.json();
+    return obj.id;
+  } catch (error) {
+    throw Error(error.message);
+  }
+};
+
+export const getProjects = async userID => {
+  const url = 'http://:localhost:3001';
+  const path = `/api/v1/users/${userID}/projects`;
+  const response = await fetch(url + path);
+  try {
+    if (!response.ok) {
+      const message = await response.json();
+      throw new Error(message.error);
+    }
+    return await response.json();
+  } catch ({ message }) {
+    throw Error(message);
+  }
+};
+
+export const getProject = async (userID, projectID) => {
+  const url = 'http://:localhost:3001';
+  const path = `/api/v1/users/${userID}/projects/${projectID}`;
+  const response = await fetch(url + path);
+  try {
+    if (!response.ok) {
+      const message = await response.json();
+      throw new Error(message.error);
+    }
+    return await response.json();
+  } catch ({ message }) {
+    throw Error(message);
+  }
+};
+
+export const createProject = async (user_id, name, description) => {
+  const url = 'http://:localhost:3001';
+  const path = '/api/v1/users/projects/new';
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ user_id, name, description })
+  };
+  const response = await fetch(url + path, options);
+  try {
+    if (!response.ok) {
+      const message = await response.json();
+      throw new Error(message.error);
+    }
+    const obj = await response.json();
+    return obj[0];
+  } catch ({ message }) {
+    throw Error(message);
+  }
+};
+
+export const editProject = async (projectID, name, description) => {
+  const url = 'http://:localhost:3001';
+  const path = `/api/v1/users/projects/${projectID}/edit`;
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ name, description })
+  };
+  const response = await fetch(url + path, options);
+  try {
+    if (!response.ok) {
+      const message = await response.json();
+      throw new Error(message.error);
+    }
+    const obj = await response.json();
+    return obj[0];
+  } catch ({ message }) {
+    throw Error(message);
+  }
+};
+
+export const deleteProject = async (userID, projectID) => {
+  const url = 'http://:localhost:3001';
+  const path = `/api/v1/users/${userID}/projects/${projectID}`;
+  const response = await fetch(url + path);
+  try {
+    if (!response.ok) {
+      const message = await response.json();
+      throw new Error(message.error);
+    }
+    return await response.json();
+  } catch ({ message }) {
+    throw Error(message);
+  }
+};
+
+export const getPalettes = async userID => {
+  const url = 'http://:localhost:3001';
+  const path = `/api/v1/users/${userID}/palettes`;
+  const response = await fetch(url + path);
+  try {
+    if (!response.ok) {
+      const message = await response.json();
+      throw new Error(message.error);
+    }
+    return await response.json();
+  } catch ({ message }) {
+    throw Error(message);
+  }
+};
+
+export const createPalette = async (projectID, name, colors) => {
+  const url = 'http://:localhost:3001';
+  const path = `/api/v1/users/projects/${projectID}/palettes`;
+  const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ name, colors })
+  };
+  const response = await fetch(url + path, options);
+  try {
+    if (!response.ok) {
+      const message = await response.json();
+      throw new Error(message.error);
+    }
+    return await response.json();
+  } catch ({ message }) {
+    throw Error(message);
+  }
+};
+
+export const deletePalette = async (projectID, paletteID) => {
+  const url = 'http://:localhost:3001';
+  const path = `/api/v1/projects/${projectID}/palettes/${paletteID}`;
+  const response = await fetch(url + path);
+  try {
+    if (!response.ok) {
+      const message = await response.json();
+      throw new Error(message.error);
+    }
+    return await response.json();
+  } catch ({ message }) {
+    throw Error(message);
+  }
+};
