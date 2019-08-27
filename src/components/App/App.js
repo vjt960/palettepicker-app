de820d915c920c94498f11a416edf09dad5ba5a5
Turing School <kratok08@gmail.com>
2019-08-27 12:28:43 -0600

    Write git testing comments

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,7 +6,9 @@ import PaletteEditor from "../PaletteEditor/PaletteEditor";
 import SavePopup from "../../containers/SavePopup/SavePopup";
 import userPopup from "../../containers/userPopup/userPopup";
 import { Route } from "react-router-dom";
-
+/*
+  Primary computer broke, had to move everything over the a temporary laptop and rebuild from the ground up. The local git accounts were not the same as the remote which caused an error at push. Went through multiple git rebases and amends to resolve the conflicts, push up a new branch and fixed the merge conflicts. This branch is simply to test that everything now works correctly and future issues are resolved.
+*/
 function App() {
   return (
     <section className="App">
