16ca788fe882532123e214c8828e3113728f82d3
Turing School <kratok08@gmail.com>
2019-08-28 15:03:06 -0600

    Test EditBarFull

--- src/components/EditBarFull/EditBarFull.test.js
+++ src/components/EditBarFull/EditBarFull.test.js
@@ -1,8 +1,23 @@
 import React from "react";
 import EditBarFull from "./EditBarFull";
+import { shallow } from "enzyme";
+import { shallowToJson } from "enzyme-to-json";
 
 describe("EditBarFull", () => {
-  it("should pass", () => {
-    expect(true).toBe(true);
+  it("should match the snapshot with all data passed in correctly", () => {
+    const wrapper = shallow(<EditBarFull updateColors={jest.fn()} />);
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+  it("should updateColors when the form onSubmit event occurs", () => {
+    const mockUpdateColors = jest.fn();
+    const wrapper = shallow(<EditBarFull updateColors={mockUpdateColors} />);
+    const form = wrapper.find(".edits-form");
+
+    const children = form
+      .render()
+      .children()
+      .children();
+    form.simulate("submit", { target: { children } });
+    expect(mockUpdateColors).toHaveBeenCalled();
   });
 });
