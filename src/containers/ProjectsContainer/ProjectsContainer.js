b18fbb8cf86817b9729f72e3a039ac916159dfd3
Vinton Teo <taulago@ymail.com>
2019-08-27 23:03:19 -0600

    Map userDetails to props.

--- src/containers/ProjectsContainer/ProjectsContainer.js
+++ src/containers/ProjectsContainer/ProjectsContainer.js
@@ -11,79 +11,82 @@ class ProjectsContainer extends Component {
   };
 
   componentDidMount() {
-    this.props.addUserProjects([
-      {
-        projectTitle: "--Send to Steve",
-        projectId: "asd3213ey1u",
-        projectDesc: "Palettes that Steve requested ...",
-        palettes: [
-          {
-            paletteTitle: "Ok here is title",
-            paletteColors: ["#45CD12", "#DFD442", "#4E3211"],
-            paletteId: "asd3a1DD3sd"
-          },
-          {
-            paletteTitle: "Idea - 2 Colors",
-            paletteColors: ["#123456"],
-            paletteId: "eeEE123D3sd"
-          },
-          {
-            paletteTitle: "Serene",
-            paletteColors: ["#FEDCBA", "#45CD12", "#3A3A3A", "#123123"],
-            paletteId: "f321DFDff3sd"
-          },
-          {
-            paletteTitle: "-- Send to Steve",
-            paletteColors: [
-              "#FEDCBA",
-              "#FEDFED",
-              "#ABCABC",
-              "#123123",
-              "#3A3A3A"
-            ],
-            paletteId: "eee23aOO3sd"
-          },
-          {
-            paletteTitle: "Iteration 4",
-            paletteColors: [
-              "#FEDCBA",
-              "#FFFFDD",
-              "#45CD12",
-              "#AABBE1",
-              "#3A3A3A"
-            ],
-            paletteId: "13ASeeU98MN"
-          },
-          {
-            paletteTitle: "- Two Potential Options -",
-            paletteColors: ["#AFFFDD", "#1A3A3A"],
-            paletteId: "13AS3U98MN"
-          }
-        ]
-      },
-      {
-        projectTitle: "My Favorites!",
-        projectId: "3ykuyOP23eeE",
-        projectDesc: "A list of my favorite palettes.",
-        palettes: [
-          {
-            paletteTitle: "For Mary",
-            paletteColors: ["#568111", "#512612", "#EA31EA"],
-            paletteId: "oi112uTR3y"
-          },
-          {
-            paletteTitle: "Idea - ???",
-            paletteColors: ["#123456", "#99EE32"],
-            paletteId: "833AS3U99M"
-          },
-          {
-            paletteTitle: "Iteration 2",
-            paletteColors: ["#FEDCBA", "#FFFFDD", "#AABBE1", "#3A3A3A"],
-            paletteId: "13AS3U98MN"
-          }
-        ]
-      }
-    ]);
+    const { userDetails } = this.props;
+    this.setState({ userId: userDetails.id, username: userDetails.username });
+
+    // this.props.addUserProjects([
+    //   {
+    //     projectTitle: "--Send to Steve",
+    //     projectId: "asd3213ey1u",
+    //     projectDesc: "Palettes that Steve requested ...",
+    //     palettes: [
+    //       {
+    //         paletteTitle: "Ok here is title",
+    //         paletteColors: ["#45CD12", "#DFD442", "#4E3211"],
+    //         paletteId: "asd3a1DD3sd"
+    //       },
+    //       {
+    //         paletteTitle: "Idea - 2 Colors",
+    //         paletteColors: ["#123456"],
+    //         paletteId: "eeEE123D3sd"
+    //       },
+    //       {
+    //         paletteTitle: "Serene",
+    //         paletteColors: ["#FEDCBA", "#45CD12", "#3A3A3A", "#123123"],
+    //         paletteId: "f321DFDff3sd"
+    //       },
+    //       {
+    //         paletteTitle: "-- Send to Steve",
+    //         paletteColors: [
+    //           "#FEDCBA",
+    //           "#FEDFED",
+    //           "#ABCABC",
+    //           "#123123",
+    //           "#3A3A3A"
+    //         ],
+    //         paletteId: "eee23aOO3sd"
+    //       },
+    //       {
+    //         paletteTitle: "Iteration 4",
+    //         paletteColors: [
+    //           "#FEDCBA",
+    //           "#FFFFDD",
+    //           "#45CD12",
+    //           "#AABBE1",
+    //           "#3A3A3A"
+    //         ],
+    //         paletteId: "13ASeeU98MN"
+    //       },
+    //       {
+    //         paletteTitle: "- Two Potential Options -",
+    //         paletteColors: ["#AFFFDD", "#1A3A3A"],
+    //         paletteId: "13AS3U98MN"
+    //       }
+    //     ]
+    //   },
+    //   {
+    //     projectTitle: "My Favorites!",
+    //     projectId: "3ykuyOP23eeE",
+    //     projectDesc: "A list of my favorite palettes.",
+    //     palettes: [
+    //       {
+    //         paletteTitle: "For Mary",
+    //         paletteColors: ["#568111", "#512612", "#EA31EA"],
+    //         paletteId: "oi112uTR3y"
+    //       },
+    //       {
+    //         paletteTitle: "Idea - ???",
+    //         paletteColors: ["#123456", "#99EE32"],
+    //         paletteId: "833AS3U99M"
+    //       },
+    //       {
+    //         paletteTitle: "Iteration 2",
+    //         paletteColors: ["#FEDCBA", "#FFFFDD", "#AABBE1", "#3A3A3A"],
+    //         paletteId: "13AS3U98MN"
+    //       }
+    //     ]
+    //   }
+    // ]);
   }
 
   removeProject = id => {
@@ -123,7 +126,8 @@ class ProjectsContainer extends Component {
 }
 
 const mapStateToProps = store => ({
-  userProjects: store.userProjects
+  userProjects: store.userProjects,
+  userDetails: store.userDetails
 });
 
 const mapDispatchToProps = dispatch => ({
