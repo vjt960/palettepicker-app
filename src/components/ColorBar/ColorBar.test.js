14be916cf23a09043abd701ea8e5d6b2f2928aa1
Turing School <kratok08@gmail.com>
2019-08-28 16:39:32 -0600

    Full test coverage on ColorBar

---
+++ src/components/ColorBar/ColorBar.test.js
@@ -0,0 +1,50 @@
+import React from "react";
+import ColorBar from "./ColorBar";
+import { shallow } from "enzyme";
+import { shallowToJson } from "enzyme-to-json";
+
+describe("ColorBar", () => {
+  let wrapper;
+  const handleLockStatus = jest.fn();
+  beforeEach(() => {
+    wrapper = shallow(
+      <ColorBar
+        color={{ hex: "#fff", locked: false }}
+        number={4}
+        handleLockStatus={handleLockStatus}
+      />
+    );
+  });
+  it("should match the snapshot", () => {
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+  it("should display the lock icon if the color is locked", () => {
+    const wrapper = shallow(
+      <ColorBar color={{ hex: "#fff", locked: true }} number={4} />
+    );
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+  it("should invoke the handleLockStatus function if the '.lock-button' (while locked:false) is clicked", () => {
+    const button = wrapper.find(".lock-button");
+    button.simulate("click");
+    expect(handleLockStatus).toHaveBeenCalled();
+  });
+  it("should invoke the handleLockStatus function if the '.lock-button' (while locked:true) is clicked", () => {
+    const wrapper = shallow(
+      <ColorBar
+        color={{ hex: "#fff", locked: true }}
+        number={4}
+        handleLockStatus={handleLockStatus}
+      />
+    );
+    const button = wrapper.find(".lock-button");
+    button.simulate("click");
+    expect(handleLockStatus).toHaveBeenCalled();
+  });
+  it("should render the hex codes vertically if the vRotate prop is passed in", () => {
+    const wrapper = shallow(
+      <ColorBar color={{ hex: "#fff", locked: true }} vRotate={true} />
+    );
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+});
