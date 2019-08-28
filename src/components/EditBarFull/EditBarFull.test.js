a5a20f519466e1a3b7b19ebfaaed19ae08bb543d
Turing School <kratok08@gmail.com>
2019-08-28 16:09:40 -0600

    simulate variation and colorScheme click events

--- src/components/EditBarFull/EditBarFull.test.js
+++ src/components/EditBarFull/EditBarFull.test.js
@@ -4,20 +4,52 @@ import { shallow } from "enzyme";
 import { shallowToJson } from "enzyme-to-json";
 
 describe("EditBarFull", () => {
+  let wrapper;
+  const mockUpdateColors = jest.fn();
+  beforeEach(() => {
+    wrapper = shallow(
+      <EditBarFull updateColors={mockUpdateColors} hueLocked={false} />
+    );
+  });
   it("should match the snapshot with all data passed in correctly", () => {
-    const wrapper = shallow(<EditBarFull updateColors={jest.fn()} />);
     expect(shallowToJson(wrapper)).toMatchSnapshot();
   });
   it("should updateColors when the form onSubmit event occurs", () => {
-    const mockUpdateColors = jest.fn();
-    const wrapper = shallow(<EditBarFull updateColors={mockUpdateColors} />);
     const form = wrapper.find(".edits-form");
-
     const children = form
       .render()
       .children()
       .children();
     form.simulate("submit", { target: { children } });
     expect(mockUpdateColors).toHaveBeenCalled();
+  });
+  it("should render the 'Lock' button when the hueLocked prop is false", () => {
+    const lockedButton = wrapper
+      .find("button")
+      .at(0)
+      .text();
+    expect(lockedButton).toEqual("Lock");
+  });
+  it("should render the 'Unlock' button when the hueLocked prop is true", () => {
+    const wrapper = shallow(<EditBarFull hueLocked={true} />);
+    const unLockedButton = wrapper
+      .find("button")
+      .at(0)
+      .text();
+    expect(unLockedButton).toEqual("Unlock");
+  });
+  it("should invoke the updateColorScheme function when the colorScheme-selection inputs are clicked", () => {
+    const updateColorScheme = jest.fn();
+    wrapper = shallow(<EditBarFull updateColorScheme={updateColorScheme} />);
+    const colorSchemeBtn = wrapper.find('[name="colorScheme-selection"]').at(0);
+    colorSchemeBtn.simulate("click");
+    expect(updateColorScheme).toHaveBeenCalled();
+  });
+  it("should invoke the updateVariation function when the variation-selection inputs are clicked", () => {
+    const updateVariation = jest.fn();
+    wrapper = shallow(<EditBarFull updateVariation={updateVariation} />);
+    const variationBtn = wrapper.find('[name="variation-selection"]').at(0);
+    variationBtn.simulate("click");
+    expect(updateVariation).toHaveBeenCalled();
   });
 });
