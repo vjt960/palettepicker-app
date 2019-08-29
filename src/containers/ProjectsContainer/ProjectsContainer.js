b472a8d41ba3c4bb2e30e07991767b2848ea458c
Vinton Teo <taulago@ymail.com>
2019-08-28 20:31:59 -0600

    Add removeProject method.

--- src/containers/ProjectsContainer/ProjectsContainer.js
+++ src/containers/ProjectsContainer/ProjectsContainer.js
@@ -2,92 +2,30 @@ import React, { Component } from "react";
 import Project from "../../components/Project/Project";
 import { TransitionGroup } from "react-transition-group";
 import * as actions from "../../_redux/actions/index";
+import { deleteProject, deletePalette } from "../../_utilities/apiCalls";
 import { connect } from "react-redux";
 import "./ProjectsContainer.scss";
 
 class ProjectsContainer extends Component {
   state = {
-    user: { userId: "123ddEad2199E", username: "Elliot" }
+    user: { userId: "123ddEad2199E", username: "Elliot" },
+    error: ""
   };
 
   componentDidMount() {
     const { userDetails } = this.props;
     this.setState({ userId: userDetails.id, username: userDetails.username });
-
-    // this.props.addUserProjects([
-    //   {
-    //     projectTitle: "--Send to Steve",
-    //     projectId: "asd3213ey1u",
-    //     projectDesc: "Palettes that Steve requested ...",
-    //     palettes: [
-    //       {
-    //         paletteTitle: "Ok here is title",
-    //         paletteColors: ["#45CD12", "#DFD442", "#4E3211"],
-    //         paletteId: "asd3a1DD3sd"
-    //       },
-    //       {
-    //         paletteTitle: "Idea - 2 Colors",
-    //         paletteColors: ["#123456"],
-    //         paletteId: "eeEE123D3sd"
-    //       },
-    //       {
-    //         paletteTitle: "Serene",
-    //         paletteColors: ["#FEDCBA", "#45CD12", "#3A3A3A", "#123123"],
-    //         paletteId: "f321DFDff3sd"
-    //       },
-    //       {
-    //         paletteTitle: "-- Send to Steve",
-    //         paletteColors: [
-    //           "#FEDCBA",
-    //           "#FEDFED",
-    //           "#ABCABC",
-    //           "#123123",
-    //           "#3A3A3A"
-    //         ],
-    //         paletteId: "eee23aOO3sd"
-    //       },
-    //       {
-    //         paletteTitle: "Iteration 4",
-    //         paletteColors: [
-    //           "#FEDCBA",
-    //           "#FFFFDD",
-    //           "#45CD12",
-    //           "#AABBE1",
-    //           "#3A3A3A"
-    //         ],
-    //         paletteId: "13ASeeU98MN"
-    //       },
-    //       {
-    //         paletteTitle: "- Two Potential Options -",
-    //         paletteColors: ["#AFFFDD", "#1A3A3A"],
-    //         paletteId: "13AS3U98MN"
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     projectTitle: "My Favorites!",
-    //     projectId: "3ykuyOP23eeE",
-    //     projectDesc: "A list of my favorite palettes.",
-    //     palettes: [
-    //       {
-    //         paletteTitle: "For Mary",
-    //         paletteColors: ["#568111", "#512612", "#EA31EA"],
-    //         paletteId: "oi112uTR3y"
-    //       },
-    //       {
-    //         paletteTitle: "Idea - ???",
-    //         paletteColors: ["#123456", "#99EE32"],
-    //         paletteId: "833AS3U99M"
-    //       },
-    //       {
-    //         paletteTitle: "Iteration 2",
-    //         paletteColors: ["#FEDCBA", "#FFFFDD", "#AABBE1", "#3A3A3A"],
-    //         paletteId: "13AS3U98MN"
-    //       }
-    //     ]
-    //   }
-    // ]);
   }
+
+  removeProject = async id => {
+    try {
+      const { userDetails, removeUserProject } = this.props;
+      await deleteProject(userDetails.id, id);
+      removeUserProject(id);
+    } catch ({ message }) {
+      this.setState({ error: message });
+    }
+  };
 
   removePalette = async (projectId, paletteId) => {
     await deletePalette(projectId, paletteId);
@@ -134,6 +72,7 @@ const mapStateToProps = store => ({
 
 const mapDispatchToProps = dispatch => ({
   addUserProjects: projects => dispatch(actions.addUserProjects(projects)),
+  removeUserProject: id => dispatch(actions.removeUserProject(id)),
   removePalette: (projectId, paletteId) =>
     dispatch(actions.removePalette(projectId, paletteId))
 });
