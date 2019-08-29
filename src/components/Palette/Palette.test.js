a18c58a621cec029582dc7326ac43c674fdef60b
Turing School <kratok08@gmail.com>
2019-08-28 20:08:43 -0600

    Fully test Palette while awaiting updated variant

---
+++ src/components/Palette/Palette.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Palette } from "./Palette";
+import PaletteEditor from "../PaletteEditor/PaletteEditor";
+import { shallow } from "enzyme";
+import { shallowToJson } from "enzyme-to-json";
+
+describe("Palette", () => {
+  let wrapper;
+  const historyMock = { push: jest.fn() };
+  beforeEach(() => {
+    wrapper = shallow(<Palette history={historyMock} colors={["#111"]} />);
+  });
+  it("should match the snapshot", () => {
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+  it("should invoke the openEditor function when clicked", () => {
+    jest;
+    const button = wrapper.find(".color");
+    button.simulate("click");
+    expect(historyMock.push.mock.calls[0]).toEqual(["edit-palette"]);
+  });
+  xit("should invoke the onClick function when the '.palette-delete' is clicked", () => {
+    // UPDATE WITH MERGE
+    const button = wrapper.find(".palette-delete");
+    button.simulate("click");
+    expect(button).toHaveBeenCalled();
+  });
+});
