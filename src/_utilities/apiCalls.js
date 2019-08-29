1db4086566fa7366596e41e1fa545e621f97eacc
Vinton Teo <taulago@ymail.com>
2019-08-29 00:18:34 -0600

    Remove unused fetch calls.

--- src/_utilities/apiCalls.js
+++ src/_utilities/apiCalls.js
@@ -53,21 +53,6 @@ export const getProjects = async userID => {
   }
 };
 
-export const getProject = async (userID, projectID) => {
-  const url = "http://localhost:3001";
-  const path = `/api/v1/users/${userID}/projects/${projectID}`;
-  const response = await fetch(url + path);
-  try {
-    if (!response.ok) {
-      const message = await response.json();
-      throw new Error(message.error);
-    }
-    return await response.json();
-  } catch ({ message }) {
-    throw Error(message);
-  }
-};
-
 export const createProject = async (user_id, name, description) => {
   const url = "http://localhost:3001";
   const path = "/api/v1/users/projects/new";
@@ -89,47 +74,11 @@ export const createProject = async (user_id, name, description) => {
   }
 };
 
-export const editProject = async (projectID, name, description) => {
-  const url = "http://localhost:3001";
-  const path = `/api/v1/users/projects/${projectID}/edit`;
-  const options = {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ name, description })
-  };
-  const response = await fetch(url + path, options);
-  try {
-    if (!response.ok) {
-      const message = await response.json();
-      throw new Error(message.error);
-    }
-    const obj = await response.json();
-    return obj[0];
-  } catch ({ message }) {
-    throw Error(message);
-  }
-};
-
 export const deleteProject = async (userID, projectID) => {
   const url = "http://localhost:3001";
   const path = `/api/v1/users/${userID}/projects/${projectID}`;
   const options = { method: "DELETE" };
   const response = await fetch(url + path, options);
-  try {
-    if (!response.ok) {
-      const message = await response.json();
-      throw new Error(message.error);
-    }
-    return await response.json();
-  } catch ({ message }) {
-    throw Error(message);
-  }
-};
-
-export const getPalettes = async userID => {
-  const url = "http://localhost:3001";
-  const path = `/api/v1/users/${userID}/palettes`;
-  const response = await fetch(url + path);
   try {
     if (!response.ok) {
       const message = await response.json();
